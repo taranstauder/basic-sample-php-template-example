ccd3582549a4220fe45d8cd5274a11550db1ce4e
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-09-01 18:33:55 -0500

    Update navigation.php

--- includes/navigation.php
+++ includes/navigation.php
@@ -4,10 +4,10 @@
 	    <a class="nav-link <?php if ($CURRENT_PAGE == "Index") {?>active<?php }?>" href="index.php">Home</a>
 	  </li>
 	  <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "About") {?>active<?php }?>" href="about.php">About Us</a>
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Page 2") {?>active<?php }?>" href="page2.php">Page 2</a>
 	  </li>
 	  <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "Contact") {?>active<?php }?>" href="contact.php">Contact</a>
+	    <a class="nav-link <?php if ($CURRENT_PAGE == "Page 3") {?>active<?php }?>" href="page3.php">Page 3</a>
 	  </li>
 	</ul>
 </div>
